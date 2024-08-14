e3d729d4780eda49bb985483222fd164617e4f5f
Avadhi <avadhijain2004@gmail.com>
2024-08-14 17:22:39 +0530

    Admin Header Working Fine

--- frontend/src/RootElementForAdmin.jsx
+++ frontend/src/RootElementForAdmin.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import AdminHeader from './components/Admin-Dashboard/AdminHeader'
 
 
 function RootElementForAdmin() {
     return (
         <>
+            <AdminHeader />
             <Outlet/>   
         </>
     )
