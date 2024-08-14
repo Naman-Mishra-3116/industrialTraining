c14629ee5a7c0b0a2bd71fb451f6eb6f5f781867
Naman Mishra <125729522+Naman-Mishra-3116@users.noreply.github.com>
2024-08-14 17:02:34 +0530

    DashBoard Api all working fine

--- Backend/Routes/admin.js
+++ Backend/Routes/admin.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { authenticate } from "../auth/verifyToken.js";
 import { loginFunction } from "../controllers/adminController.js";
+import {
+  availableBookings,
+  availableDoctors,
+  availableUsers,
+} from "../controllers/adminController.js";
 
 const router = Router();
 
 router.post("/add", authenticate);
 router.post("/login", loginFunction);
+router.get("/:id/allUsers", availableUsers);
+router.get("/:id/allDoctors", availableDoctors);
+router.get("/:id/allBookings", availableBookings);
 
 export { router as AdminRouter };
--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -1,5 +1,8 @@
 import { AdminUser } from "../models/AdminSchema.js";
 import jwt from "jsonwebtoken";
+import UserSchema from "../models/UserSchema.js";
+import DoctorSchema from "../models/DoctorSchema.js";
+import BookingSchema from "../models/BookingSchema.js";
 
 export const addAdminFunction = async function (req, res) {
   try {
@@ -50,5 +53,86 @@ export const loginFunction = async function (req, res) {
     }
   } catch (err) {
     res.status(200).json({ message: err.message, success: false, error: true });
+  }
+};
+
+export const availableUsers = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const isValid = await AdminUser.findOne({ _id: id });
+    if (isValid) {
+      const result = await UserSchema.find().select("-password");
+      console.log(result);
+      res.status(200).json({
+        message: "Data delivered",
+        error: false,
+        success: true,
+        data: result,
+      });
+    } else {
+      return res.status(200).json({
+        message: "You are not authorized",
+        success: false,
+        error: true,
+      });
+    }
+  } catch (error) {
+    res
+      .status(200)
+      .json({ message: error.message, success: false, error: true });
+  }
+};
+
+export const availableDoctors = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const isValid = await AdminUser.findOne({ _id: id });
+    if (isValid) {
+      const result = await DoctorSchema.find().select("-password");
+      console.log(result);
+      res.status(200).json({
+        message: "Data delivered",
+        error: false,
+        success: true,
+        data: result,
+      });
+    } else {
+      return res.status(200).json({
+        message: "You are not authorized",
+        success: false,
+        error: true,
+      });
+    }
+  } catch (error) {
+    res
+      .status(200)
+      .json({ message: error.message, success: false, error: true });
+  }
+};
+
+export const availableBookings = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const isValid = await AdminUser.findOne({ _id: id });
+    if (isValid) {
+      const result = await BookingSchema.find();
+      console.log(result);
+      res.status(200).json({
+        message: "Data delivered",
+        error: false,
+        success: true,
+        data: result,
+      });
+    } else {
+      return res.status(200).json({
+        message: "You are not authorized",
+        success: false,
+        error: true,
+      });
+    }
+  } catch (error) {
+    res
+      .status(200)
+      .json({ message: error.message, success: false, error: true });
   }
 };
