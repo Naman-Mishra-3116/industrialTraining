b9df6f225cadd801cc967b5ee20a312235c7fee5
Naman Mishra <125729522+Naman-Mishra-3116@users.noreply.github.com>
2024-08-14 17:50:51 +0530

    Api Working Fine

--- frontend/src/components/Admin-Dashboard/DoctorsTable.jsx
+++ frontend/src/components/Admin-Dashboard/DoctorsTable.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import Error from "../Error/Error";
+import Loading from "../Loader/Loading";
+import { BASE_URL } from "../../config";
 
 function DoctorsTable() {
+  const adminData = JSON.parse(localStorage.getItem("adminData"));
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async function () {
+      try {
+        setLoading(true);
+        if (adminData.id) {
+          const resp = await fetch(
+            `${BASE_URL}/admin/${adminData.id}/allDoctors`,
+            {
+              method: "GET",
+            }
+          );
+          const { message, error, success, data } = await resp.json();
+          console.log(message, error, success, data);
+          if (message === "Data delivered" && !error && success) {
+            setData(data);
+            setLoading(false);
+          } else if (
+            message === "You are not authorized" &&
+            !error &&
+            success
+          ) {
+            setLoading(false);
+            setError(message);
+          }
+        }
+      } catch (err) {
+        setLoading(false);
+        setError(err.message);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>Doctors</div>
-  )
+    <div>
+      {error && <Error errMessage={error} />}
+      {loading && <Loading />}
+      {!error && !loading && (
+        <div>
+          {data &&
+            data.map((item, index) => {
+              return <div key={index}>{item.name}</div>;
+            })}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default DoctorsTable
+export default DoctorsTable;
--- frontend/src/components/Admin-Dashboard/Users.jsx
+++ frontend/src/components/Admin-Dashboard/Users.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { BASE_URL } from "../../config";
+import { useState } from "react";
+import Loading from "../Loader/Loading";
+import Error from "../Error/Error";
 
 function Users() {
+  const adminData = JSON.parse(localStorage.getItem("adminData"));
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async function () {
+      try {
+        setLoading(true);
+        if (adminData.id) {
+          const resp = await fetch(
+            `${BASE_URL}/admin/${adminData.id}/allUsers`,
+            {
+              method: "GET",
+            }
+          );
+          const { message, error, success, data } = await resp.json();
+          console.log(message, error, success, data);
+          if (message === "Data delivered" && !error && success) {
+            setData(data);
+            setLoading(false);
+          } else if (
+            message === "You are not authorized" &&
+            !error &&
+            success
+          ) {
+            setLoading(false);
+            setError(message);
+          }
+        }
+      } catch (err) {
+        setLoading(false);
+        setError(err.message);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>Users</div>
-  )
+    <div>
+      {error && <Error errMessage={error} />}
+      {loading && <Loading />}
+      {!error && !loading && (
+        <div>
+          {data &&
+            data.map((item, index) => {
+              return <div key={index}>{item.name}</div>;
+            })}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Users
+export default Users;
