b3f061a088efb7ba52ca94653db57a46ffbe7347
Avadhi <avadhijain2004@gmail.com>
2024-08-15 21:08:06 +0530

    Doctors Added

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import aboutImg from '../../assets/images/about.png'
+import aboutImg from '../../assets/images/about.jpg'
 import aboutCardImg from '../../assets/images/about-card.png'
 
 function About() {
@@ -12,7 +12,7 @@ function About() {
                 <div className="flex justify-between gap-[50px] lg:[130px] xl:gap-0 flex-col lg:flex-row  ">
                     {/* ======About Image========= */}
                     <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1 ">
-                        <img src={aboutImg} alt="" />
+                        <img className='rounded-xl w-[600px] h-[600px]' src={aboutImg} alt="" />
                         <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px] right-[-30%] md:right-[-7%] lg:right-[22%] ">
                             <img src={aboutCardImg} alt="" />
                         </div>
